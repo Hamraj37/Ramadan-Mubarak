497f3e4bc2ca430e2a9ae3cdfdbaefd9026133e3
Hamraj37 <>
2025-02-07 22:32:25 +0530

    Last Sync: 2025-02-07 17:02:25 (Mobile)

--- js/script.js
+++ js/script.js
@@ -24,3 +24,17 @@ function toggleCheck() {
         setDark(false);
     }
 }
+
+function start () {
+  // (PART A) GET SPLASH SCREEN
+  let splash = document.getElementById("splash");
+ 
+  // (PART B) PLAY BGM & REMOVE SPLASH SCREEN AFTER FADE IN
+  splash.addEventListener("transitionend", () => {
+    document.getElementById("bgm").play();
+    splash.remove();
+  });
+ 
+  // (PART C) GO!
+  splash.classList.add("hide");
+}
